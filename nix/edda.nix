72804307062f9751fc4bf8df776a90a744a34817
TheEdward162 <theedward162@gmail.com>
2025-05-20 01:44:32 +0200

    update nix

--- nix/edda.nix
+++ nix/edda.nix
@@ -9,11 +9,12 @@
 		home.packages = with pkgs; [
 			eza htop ncdu git curl direnv just
 			ripgrep httpie p7zip jq jless
+			helix
 			nerd-fonts.fira-mono
 			fortune cowsay
 			pass
 		]
-		++ lib.lists.optionals local-os.gui [ firefox zed-editor python313 telegram-desktop ]
+		++ lib.lists.optionals local-os.gui [ firefox zed-editor python313 telegram-desktop mpv ]
 		++ lib.lists.optionals (local-os.gui && osClass == "darwin") [ iterm2 ];
 		fonts.fontconfig.enable = true;
 
@@ -27,49 +28,53 @@
 			}
 			(lib.mkIf local-os.gui { code = "zeditor"; })
 			(lib.mkIf (osClass == "darwin") {
-				needu = "nix run nix-darwin -- switch --flake ~/Documents/infrastructure/metal/os#${local-os.hostname}";
+				needu-os = "nix flake update --flake ~/Documents/infrastructure/metal/os";
+				needu-apply = "darwin-rebuild switch --flake ~/Documents/infrastructure/metal/os#${local-os.hostname}";
 			})
 		];
+		home.sessionVariables = {
+			"EDITOR" = "hx";
+		};
 		programs.fish = {
 			enable = true;
 			interactiveShellInit = ''
 				function fish_prompt
-				set -l last_status $status
+					set -l last_status $status
 
-				set -g __fish_git_prompt_showdirtystate	1
-				set -g __fish_git_prompt_showuntrackedfiles 1
+					set -g __fish_git_prompt_showdirtystate	1
+					set -g __fish_git_prompt_showuntrackedfiles 1
 
-				set -f lambda_colors green red
-				if fish_is_root_user
-				set -f lambda_colors magenta red
-				end
+					set -f lambda_colors green red
+					if fish_is_root_user
+						set -f lambda_colors magenta red
+					end
 
-				if test $last_status -eq 0
-				set -f lambda_color $lambda_colors[1]
-				else
-				set -f lambda_color $lambda_colors[2]
-				end
+					if test $last_status -eq 0
+						set -f lambda_color $lambda_colors[1]
+					else
+						set -f lambda_color $lambda_colors[2]
+					end
 
-				set -f tags (string join ',' $prompt_tags)
-				if test -n "$tags"
-				set -f tags " {$tags}"
-				end
+					set -f tags (string join ',' $prompt_tags)
+					if test -n "$tags"
+						set -f tags " {$tags}"
+					end
 
-				echo "$(set_color yellow)$PWD""$(set_color --bold white)$(fish_git_prompt)$(set_color normal)""$(set_color white)$tags$(set_color normal)""$(set_color --bold green) λ$(set_color normal) "
+					echo "$(set_color yellow)$PWD""$(set_color --bold white)$(fish_git_prompt)$(set_color normal)""$(set_color white)$tags$(set_color normal)""$(set_color --bold green) λ$(set_color normal) "
 				end
 
 				function fish_right_prompt
-				set -l last_status $status
+					set -l last_status $status
 
-				printf '[%s] ' $last_status
-				set_color green
-				date +'%H:%M:%S'
-				set_color normal
+					printf '[%s] ' $last_status
+					set_color green
+					date +'%H:%M:%S'
+					set_color normal
 				end
 
 				function fish_greeting
-				set joke (curl --silent --max-time 1 -H 'Accept: text/plain' https://icanhazdadjoke.com/ || fortune)
-				echo $joke | cowsay -f moose.cow
+					set joke (curl --silent --max-time 1 -H 'Accept: text/plain' https://icanhazdadjoke.com/ || fortune)
+					echo $joke | cowsay -f moose.cow
 				end
 
 				direnv hook fish | source
